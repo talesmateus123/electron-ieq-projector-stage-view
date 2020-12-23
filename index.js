80313aea4b1c78aba2dee2e9fb48288d616ec593
Tales Ferreira <talesmateus1999@hotmail.com>
2020-12-23 00:28:19 -0400

    Implemented restore location window

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ const Store = require('./store')
 const store = new Store({
   configName: 'user-preferences',
   defaults: {
-    windowBounds: { width: 900, height: 300 }
+    windowBounds: { width: 900, height: 300 },
+    location: { x: 0, y: 0 }
   }
 })
 
 function createWindow () {
-  let { width, height } = store.get('windowBounds');
+  let { width, height } = store.get('windowBounds')
+  let { x, y } = store.get('location')
+  console.log(x, y)
   const win = new BrowserWindow({
     width: width,
     height: height,
+    x: x,
+    y: y,
     frame: false,
     icon: currentPlatform === 'win32' ? 'assets/win/ieq-icon.ico' : currentPlatform === 'linux' ? 'assets/linux/ieq-icon.png' : 'assets/osx/ieq-icon.png',
     webPreferences: {
@@ -28,6 +33,11 @@ function createWindow () {
     let { width, height } = win.getBounds();
     // Now that we have them, save them using the `set` method.
     store.set('windowBounds', { width, height });
+  })
+
+  win.on('move', function() {
+    let [ x, y ] = win.getPosition()
+    store.set('location', { x, y });
   })
 
   win.loadFile('stage-view/index.html')
--- shortcutsConfig.js
+++ shortcutsConfig.js
@@ -1,17 +1,7 @@
 const { BrowserWindow, globalShortcut } = require('electron')
 
-function closeWindow() {
-	const focusedWindow = BrowserWindow.getFocusedWindow()
-	if (focusedWindow)
-		focusedWindow.close()
-}
 function register() {
     globalShortcut.register('f5', function() {
-		const focusedWindow = BrowserWindow.getFocusedWindow()
-		if (focusedWindow)
-			BrowserWindow.getFocusedWindow().reload()
-	})
-	globalShortcut.register('CommandOrControl+R', function() {
 		const focusedWindow = BrowserWindow.getFocusedWindow()
 		if (focusedWindow)
 			BrowserWindow.getFocusedWindow().reload()
@@ -29,12 +19,6 @@ function register() {
 			const webFrame = focusedWindow.webContents
 			webFrame.getZoomFactor() > 0.2 && webFrame.setZoomFactor(webFrame.getZoomFactor() - 0.1)
 		}
-	})
-	globalShortcut.register('CommandOrControl+W', function() {
-		closeWindow()
-	})
-	globalShortcut.register('Esc', function() {
-		closeWindow()
 	})
 }
 
